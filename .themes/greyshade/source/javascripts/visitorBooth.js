fdd3d638c9dfaed40611d704bc29ef43a9c7b5c6
Xavier Cambar <xavier.cambar@lecoffre.net>
2012-11-28 20:58:20 +0100

    Fixed the fake metadataloaded evnt

--- .themes/greyshade/source/javascripts/visitorBooth.js
+++ .themes/greyshade/source/javascripts/visitorBooth.js
@@ -1,8 +1,14 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.requestAnimationFrame = window.requestAnimationFrame ||
+                               window.webkitRequestAnimationFrame ||
+                               window.mozRequestAnimationFrame ||
+                               window.msRequestAnimationFrame ||
+                               window.oRequestAnimationFrame;
 window.URL = window.URL || window.webkitURL;
 
-function visitorsBooth (conf) {
+function visitorsBooth(conf) {
+  "use strict";
   var canvas = document.createElement('canvas');
   var shots = document.getElementById(conf.destEltId);
   var video = document.getElementById(conf.videoEltId);
@@ -18,37 +24,42 @@ function visitorsBooth (conf) {
       }, 1);
       return;
     }
-    navigator.getUserMedia({video: true}, function(localMediaStream) {
+    navigator.getUserMedia({video: true}, function (localMediaStream) {
       stream = localMediaStream;
       video.autoplay = true;
       video.src = window.URL.createObjectURL(localMediaStream);
 
       // Too bad loadedmetadata event doesn't work :-/
-      setTimeout(function(evt) {
-        video.style.display = "block";
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        fn(null, localMediaStream);
-      }, 2000);
+      var _interval = setInterval(function () {
+        if (video.videoWidth) {
+          clearInterval(_interval);
+          video.style.display = "block";
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+          fn(null, localMediaStream);
+        }
+      }, 500);
 
     }, fn);
     return video;
-  };
+  }
 
   function _shoot() {
-    ctx.drawImage(video, 0, 0);
     var _img = document.createElement('img');
-    _img.src = canvas.toDataURL('image/webp');
-    _img.width = video.width;
-    shots.appendChild(_img);
     _img.className = 'shot';
-    stream.stop();
-    video.style.display = "none";
+    shots.appendChild(_img);
+    requestAnimationFrame(function () {
+      ctx.drawImage(video, 0, 0);
+      _img.src = canvas.toDataURL('image/webp');
+      _img.width = video.width;
+      stream.stop();
+      video.style.display = "none";
+    });
     return _img;
   }
 
   return {
     start: getUM,
     shoot: _shoot
-  }
+  };
 }
